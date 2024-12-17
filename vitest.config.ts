362f635b5d6f58227a537e1284c330ce891d76d6
Louis Brann <louis-brann@users.noreply.github.com>
2024-12-17 06:33:32 -0700

    unit tests: vitest (#10)
    * configure vitest
    * pull out highlighting logic into `highlight-gear.js`
    * unit tests for `test/highlight-gear.spec.js`
    * fix regexes for pattern matching in `highlight-gear.js`

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import { WxtVitest } from 'wxt/testing';
+
+export default defineConfig({
+  plugins: [WxtVitest()],
+  test: {
+    include: ['test/**/*.spec.ts', 'test/**/*.spec.js'],
+    coverage: {
+      include: ['src']
+    }
+  }
+});

b7deaadf2c543887a33a552cfe5c5ec0218541ee
Louie Brann <louis.brann+github@gmail.com>
2024-12-17 06:04:21 -0700

    address vitest versioning

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,8 @@ import { defineConfig } from 'vitest/config';
 import { WxtVitest } from 'wxt/testing';
 
 export default defineConfig({
-  // @ts-ignore TODO type/versioning issue?
   plugins: [WxtVitest()],
   test: {
     include: ['test/**/*.spec.ts', 'test/**/*.spec.js']
-  },
-  coverage: {
-    all: true,
-    provider: 'v8',
-    include: ['src'],
   }
 });

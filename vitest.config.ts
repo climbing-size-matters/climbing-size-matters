5615c7e15702f789cad9cea03c108bc60976d125
Louie Brann <louis.brann+github@gmail.com>
2024-12-17 06:09:58 -0700

    coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { WxtVitest } from 'wxt/testing';
 export default defineConfig({
   plugins: [WxtVitest()],
   test: {
-    include: ['test/**/*.spec.ts', 'test/**/*.spec.js']
+    include: ['test/**/*.spec.ts', 'test/**/*.spec.js'],
+    coverage: {
+      include: ['src']
+    }
   }
 });

15dcf2c1a932857c05d47c3676a1e2f56848d010
Eric Warren <eswarren18@gmail.com>
2025-03-01 21:16:45 -0700

    added 0.75 equivalent highlights

--- src/entrypoints/content/gear-reg-ex.ts
+++ src/entrypoints/content/gear-reg-ex.ts
@@ -2,37 +2,29 @@
 /* Green
     Added:
         BD: 0.0, #0.0, 000, green z4
+        DMM: green dragonfly
         Imperial: 0.29-0.46 in
         Metolius: N/A
+        Metric: 7.5-11.8 mm
         Totem: N/A
     Need to add:
         BD: 0, .0
-        DMM: green dragonfly
-        Metric: 7.5-11.8 mm
         Wild Country: ??
-        BIGGER SIZES i.e., 0.75
 */
 const GREEN_COLOR = "#009e3a";
-const GREEN_PATTERN =
-  /#?0\.0|(0\.(3|4|29|3[0-9]|4[0-6])(?=\s?in|"))|([Cc]3\s?)?000(\s?[Cc]3)?|([Gg]reen)(\s[Zz]4)/;
-
+const GREEN_0_PATTERN =
+  /#?0\.0|(0\.(3|4|29|3[0-9]|4[0-6])(?=\s?in|"))|((7\.[5-9])|(8|9|10)\.[0-9]|(11\.[0-8]))(?=\s?mm)|([Cc]3\s?)?000(\s?[Cc]3)?|([Gg]reen)((\s[Zz]4)|(\s[Dd]ragonfly))/;
+const GREEN_75_PATTERN = /#?0.75|(0\.9[4-9]|1\.[0-5][0-9]|1\.6[0-2])(?=\s?in|")|(23\.[9]|2[4-9]\.[0-9]|3[0-9]\.[0-9]|40\.[0-9]|41\.[0-2])(?=\s?mm|")|([Dd]ragon)(\s2)|([Mm]astercam)(\s5)|([Bb]lack)(\s[Mm]etolius)/;
+const GREEN_6_PATTERN = /#6/
 // const RED_COLOR = "#CB1B31";
-
+// (0\.(3[4-9]|4[0-9]|5[0-4]))(?=\s?in|")
 // const YELLOW_COLOR = "#c9af19";
 
 // const BLUE_COLOR = "#1177cc";
 
 // const GRAY_COLOR = "#9ba1b7";
 
-// const CAMALOT_PATTERN = /(?<!\w)[Cc]am(-?a-?lot)?s?(?!\w)/gi; // # matches e.g. cam, Cam-a-lot
-const GEAR_SIZE_PATTERN =
-  /(?<!\w)(#(0(0|\.([12345]|(75)))?)|#[1-8])(?![\w.])/gi; // matches #00, #0, #0.1-0.75, #1-#8
-const CRACK_SIZE_PATTERN =
-  /(?<!\w)(([0-9](\.5)?)-)?[0-9](\.5)?("|cm|mm)(?!\w)/gi; // matches e.g. 3.5", 3-4"
 
-// const GEAR_COLOR = "#FF0000"; // red
-const GEAR_SIZE_COLOR = "#00FF00"; // green
-const CRACK_SIZE_COLOR = "#0000FF"; // blue
 
 type Replacement = {
   pattern: RegExp;
@@ -40,10 +32,9 @@ type Replacement = {
 };
 
 const replacements: Replacement[] = [
-  //   { pattern: CAMALOT_PATTERN, color: GEAR_COLOR },
-  { pattern: GEAR_SIZE_PATTERN, color: GEAR_SIZE_COLOR },
-  { pattern: CRACK_SIZE_PATTERN, color: CRACK_SIZE_COLOR },
-  { pattern: GREEN_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_0_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_75_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_6_PATTERN, color: GREEN_COLOR },
 ];
 
 export { replacements };

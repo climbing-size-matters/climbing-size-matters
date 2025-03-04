c74134a6d47b6bd776fd08962b7c7dcf3227ac66
Eric Warren <eswarren18@gmail.com>
2025-03-03 21:42:52 -0700

    added RegEx for red sizes

--- src/entrypoints/content/gear-reg-ex.ts
+++ src/entrypoints/content/gear-reg-ex.ts
@@ -1,24 +1,23 @@
 // Colors and patterns in reference to BD, DMM, or Wild Country gear
-/* Green
-    Added:
-        BD: 0.0, #0.0, 000, green z4
-        DMM: green dragonfly
-        Imperial: 0.29-0.46 in
-        Metolius: N/A
-        Metric: 7.5-11.8 mm
-        Totem: N/A
-    Need to add:
-        BD: 0, .0
-        Wild Country: ??
-*/
+
 const GREEN_COLOR = "#009e3a";
-const GREEN_0_PATTERN =
-  /#?0\.0|(0\.(3|4|29|3[0-9]|4[0-6])(?=\s?in|"))|((7\.[5-9])|(8|9|10)\.[0-9]|(11\.[0-8]))(?=\s?mm)|([Cc]3\s?)?000(\s?[Cc]3)?|([Gg]reen)((\s[Zz]4)|(\s[Dd]ragonfly))/;
-const GREEN_75_PATTERN =
-  /#?0.75|(0\.9[4-9]|1\.[0-5][0-9]|1\.6[0-2])(?=\s?in|")|(23\.[9]|2[4-9]\.[0-9]|3[0-9]\.[0-9]|40\.[0-9]|41\.[0-2])(?=\s?mm|")|([Dd]ragon)(\s2)|([Mm]astercam)(\s5)|([Bb]lack)(\s[Mm]etolius)/;
-const GREEN_6_PATTERN = /#6/;
-// const RED_COLOR = "#CB1B31";
-// (0\.(3[4-9]|4[0-9]|5[0-4]))(?=\s?in|")
+// Add 0, .0, WC
+const GREEN_000_PATTERN =
+  /#?0\.0|(0\.(29|3|3[0-9])(?=\s?in|"))|((7\.[5-9])|(8|9)\.[0-9]|10\.[0-2])(?=\s?mm)|([Cc]3\s?)?000(\s?[Cc]3)?|([Gr]een\s([Zz]4|[Dd]ragonfly(?:\s2)?)|[Dd]ragonfly\s1)/;
+// Add WC
+const GREEN_075_PATTERN =
+  /#?0.75|(0\.9[4-9]|1\.(?:[0-9]|[0-3][0-9])|1\.(?:4|40))(?=\s?in|")|(23\.[9]|2[4-9]\.[0-9]|3[0-9]\.[0-9]|40\.[0-9]|41\.[0-2])(?=\s?mm|")|([Dd]ragon)(\s2)|([Mm]astercam)(\s5)|([Bb]lack)(\s[Mm]etolius)/;
+// Add WC
+const GREEN_600_PATTERN = /#6/;
+
+const RED_COLOR = "#CB1B31";
+// .1, WC
+const RED_010_PATTERN =
+  /#?0\.1|(0\.(4|4[0-9]|5[0-4])(?=\s?in|"))|((10\.[3-9])|11\.[0-9]|(11\.[0-8]))(?=\s?mm)|([Cc]3\s?)?00(\s?[Cc]3)?|([Rr]ed\s([Zz]4|[Dd]ragonfly(?:\s2)?)|[Dd]ragonfly\s2)|([Gr][ae]y)(\s[Mm]etolius)|([Bb]lack)(\s[Aa]lien)/;
+const RED_100_PATTERN =
+  /#?0.75|(0\.9[4-9]|1\.(?:[0-9]|[0-3][0-9])|1\.(?:4|40))(?=\s?in|")|(23\.[9]|2[4-9]\.[0-9]|3[0-9]\.[0-9]|40\.[0-9]|41\.[0-2])(?=\s?mm|")|([Dd]ragon)(\s2)|([Mm]astercam)(\s5)|([Bb]lack)(\s[Mm]etolius)/;
+// const RED_700_PATTERN = /#7/;
+
 // const YELLOW_COLOR = "#c9af19";
 
 // const BLUE_COLOR = "#1177cc";
@@ -31,9 +30,11 @@ type Replacement = {
 };
 
 const replacements: Replacement[] = [
-  { pattern: GREEN_0_PATTERN, color: GREEN_COLOR },
-  { pattern: GREEN_75_PATTERN, color: GREEN_COLOR },
-  { pattern: GREEN_6_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_000_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_075_PATTERN, color: GREEN_COLOR },
+  { pattern: GREEN_600_PATTERN, color: GREEN_COLOR },
+  { pattern: RED_010_PATTERN, color: RED_COLOR },
+  { pattern: RED_100_PATTERN, color: RED_COLOR },
 ];
 
 export { replacements };

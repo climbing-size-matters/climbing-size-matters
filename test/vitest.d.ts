d4a97699b7655265346adda53dd8d42502ceb294
Louie Brann <louis.brann+github@gmail.com>
2025-03-06 11:14:32 -0500

    formatting

--- test/vitest.d.ts
+++ test/vitest.d.ts
@@ -8,7 +8,7 @@ interface CustomMatchers<R = unknown> {
 declare module "vitest" {
   // TODO: avoid eslint disable?
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Assertion<T> extends CustomMatchers<T> { }
+  interface Assertion<T> extends CustomMatchers<T> {}
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends CustomMatchers { }
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
 }

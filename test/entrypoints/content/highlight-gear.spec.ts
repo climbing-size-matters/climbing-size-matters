e4e45b72b85d117dda7ed69448d6b34d6271060f
Eric Warren <eswarren18@gmail.com>
2025-03-13 17:07:34 -0600

    review: revised prettier (#15)

--- test/entrypoints/content/highlight-gear.spec.ts
+++ test/entrypoints/content/highlight-gear.spec.ts
@@ -56,35 +56,111 @@ describe('higlightCams', () => {
             });
 
             it('colors', () => {
-                expect(highlightCams('red camalot')).toBeHighlightedWith('#CB1B31');
+                expect(highlightCams('red camalot')).toBeHighlightedWith(
+                    '#CB1B31'
+                );
                 expect(highlightCams('blue c4')).toBeHighlightedWith('#1177CC');
                 expect(highlightCams('gray bd')).toBeHighlightedWith('#9BA1B7');
-                expect(highlightCams('purple camalots')).toBeHighlightedWith('#614FC8');
+                expect(highlightCams('purple camalots')).toBeHighlightedWith(
+                    '#614FC8'
+                );
             });
-
         });
 
         describe('doesnt find', () => {
             it('invalid decimal sizes', () => {
                 const invalidSizes = [
-                    '#0.6', '#0.7', '#0.8', '#0.9', '#1.1', '#1.2', '#1.3', '#1.4',
-                    '#1.5', '#1.6', '#1.7', '#1.8', '#1.9', '#2.1', '#2.2', '#2.3',
-                    '#2.4', '#2.5', '#2.6', '#2.7', '#2.8', '#2.9', '#3.1', '#3.2',
-                    '#3.3', '#3.4', '#3.5', '#3.6', '#3.7', '#3.8', '#3.9', '#4.1',
-                    '#4.2', '#4.3', '#4.4', '#4.5', '#4.6', '#4.7', '#4.8', '#4.9',
-                    '#5.1', '#5.2', '#5.3', '#5.4', '#5.5', '#5.6', '#5.7', '#5.8',
-                    '#5.9', '#6.1', '#6.2', '#6.3', '#6.4', '#6.5', '#6.6', '#6.7',
-                    '#6.8', '#6.9', '#7.1', '#7.2', '#7.3', '#7.4', '#7.5', '#7.6',
-                    '#7.7', '#7.8', '#7.9', '#8.1', '#8.2', '#8.3', '#8.4', '#8.5',
-                    '#8.6', '#8.7', '#8.8', '#8.9',
+                    '#0.6',
+                    '#0.7',
+                    '#0.8',
+                    '#0.9',
+                    '#1.1',
+                    '#1.2',
+                    '#1.3',
+                    '#1.4',
+                    '#1.5',
+                    '#1.6',
+                    '#1.7',
+                    '#1.8',
+                    '#1.9',
+                    '#2.1',
+                    '#2.2',
+                    '#2.3',
+                    '#2.4',
+                    '#2.5',
+                    '#2.6',
+                    '#2.7',
+                    '#2.8',
+                    '#2.9',
+                    '#3.1',
+                    '#3.2',
+                    '#3.3',
+                    '#3.4',
+                    '#3.5',
+                    '#3.6',
+                    '#3.7',
+                    '#3.8',
+                    '#3.9',
+                    '#4.1',
+                    '#4.2',
+                    '#4.3',
+                    '#4.4',
+                    '#4.5',
+                    '#4.6',
+                    '#4.7',
+                    '#4.8',
+                    '#4.9',
+                    '#5.1',
+                    '#5.2',
+                    '#5.3',
+                    '#5.4',
+                    '#5.5',
+                    '#5.6',
+                    '#5.7',
+                    '#5.8',
+                    '#5.9',
+                    '#6.1',
+                    '#6.2',
+                    '#6.3',
+                    '#6.4',
+                    '#6.5',
+                    '#6.6',
+                    '#6.7',
+                    '#6.8',
+                    '#6.9',
+                    '#7.1',
+                    '#7.2',
+                    '#7.3',
+                    '#7.4',
+                    '#7.5',
+                    '#7.6',
+                    '#7.7',
+                    '#7.8',
+                    '#7.9',
+                    '#8.1',
+                    '#8.2',
+                    '#8.3',
+                    '#8.4',
+                    '#8.5',
+                    '#8.6',
+                    '#8.7',
+                    '#8.8',
+                    '#8.9',
                 ];
                 const validColors = [
-                    '#009E3A', '#CB1B31', '#C9AF19', '#1177CC', '#9BA1B7', '#614FC8',
+                    '#009E3A',
+                    '#CB1B31',
+                    '#C9AF19',
+                    '#1177CC',
+                    '#9BA1B7',
+                    '#614FC8',
                 ];
 
                 invalidSizes.forEach((size) => {
                     validColors.forEach((color) => {
-                        expect(highlightCams(size)).not.toBeHighlightedWith(color);
+                        expect(highlightCams(size)).not.toBeHighlightedWith(
+                            color
+                        );
                     });
                 });
             });
@@ -94,25 +170,41 @@ describe('higlightCams', () => {
     describe('metolius cam size', () => {
         describe('finds', () => {
             it('numbers', () => {
-                expect(highlightCams('1 metolius')).toBeHighlightedWith('#1177CC');
-                expect(highlightCams('#1 metolius')).toBeHighlightedWith('#1177CC');
-                expect(highlightCams('2 master cam')).toBeHighlightedWith('#1177CC');
-                expect(highlightCams('#2 master cam')).toBeHighlightedWith('#1177CC');
+                expect(highlightCams('1 metolius')).toBeHighlightedWith(
+                    '#1177CC'
+                );
+                expect(highlightCams('#1 metolius')).toBeHighlightedWith(
+                    '#1177CC'
+                );
+                expect(highlightCams('2 master cam')).toBeHighlightedWith(
+                    '#1177CC'
+                );
+                expect(highlightCams('#2 master cam')).toBeHighlightedWith(
+                    '#1177CC'
+                );
                 expect(highlightCams('3 mc')).toBeHighlightedWith('#9BA1B7');
                 expect(highlightCams('#3 mc')).toBeHighlightedWith('#9BA1B7');
             });
 
             it('colors', () => {
                 expect(highlightCams('red metolius')).toBeHighlightedWith('#');
-                expect(highlightCams('black master cam')).toBeHighlightedWith('#009E3A');
-                expect(highlightCams('green mc')).toBeHighlightedWith('#CB1B31');
+                expect(highlightCams('black master cam')).toBeHighlightedWith(
+                    '#009E3A'
+                );
+                expect(highlightCams('green mc')).toBeHighlightedWith(
+                    '#CB1B31'
+                );
             });
         });
 
         describe('doesnt find', () => {
             it('numbers', () => {
-                expect(highlightCams('9 metolius')).not.toBeHighlightedWith('#');
-                expect(highlightCams('9 master cam')).not.toBeHighlightedWith('#');
+                expect(highlightCams('9 metolius')).not.toBeHighlightedWith(
+                    '#'
+                );
+                expect(highlightCams('9 master cam')).not.toBeHighlightedWith(
+                    '#'
+                );
                 expect(highlightCams('9 mc')).not.toBeHighlightedWith('#');
             });
         });

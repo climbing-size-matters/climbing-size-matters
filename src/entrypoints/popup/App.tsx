c8a2e5e502871717b88e7e73947723e7ae1dba77
Eric Warren <eswarren18@gmail.com>
2025-03-18 16:41:55 -0600

    feature: added logo & CSS to popup

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const App: React.FC = () => {
     return (
-        <div className="w-full h-full">
-            <h1 className="text-red-400">Under Development!!</h1>
+        <div className="p-2 bg-gray-50 h-96 w-64">
+            <div className="flex justify-start items-center">
+                <img src="/icon/logo.svg" alt="logo" className="w-8 h-8 mr-2" />
+                <p className="text-lg font-bold">CSM</p>
+            </div>
+            <div className="border-b-2 border-black mx-auto my-2"></div>
+            <div className="flex justify-center">
+                <div className="text-lg">Inventory content here</div>
+            </div>
         </div>
     );
 };

9ec5d6d5a12330a8eb52b53e1f6b6181ce4a3653
Eric Warren <eswarren18@gmail.com>
2025-03-17 18:45:35 -0600

    configured index.html in popup to use tailwind.css (#20)

---
+++ src/entrypoints/popup/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className='w-full h-full'>
+            <h1 className='text-red-400'>Under Development!!</h1>
+        </div>
+    );
+};
+
+export default App;

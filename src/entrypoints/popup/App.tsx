5276a53b9dae7ab0321bb92f996bfc0e16a76ecc
Eric Warren <eswarren18@gmail.com>
2025-03-18 20:20:37 -0600

    feature: add CSS to popup (#20)

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import { useState } from 'react';
+
+import Inventory from './Inventory';
+import AddGear from './AddGear';
 
 const App: React.FC = () => {
+    const [isInventoryTab, setIsInventoryTab] = useState<boolean>(true);
+    // const [cam, setCam] = useState<string>('');
+
     return (
         <div className="p-2 bg-gray-50 h-96 w-64">
             <div className="flex justify-start items-center">
@@ -8,9 +14,21 @@ const App: React.FC = () => {
                 <p className="text-lg font-bold">CSM</p>
             </div>
             <div className="border-b-2 border-black mx-auto my-2"></div>
-            <div className="flex justify-center">
-                <div className="text-lg">Inventory content here</div>
+            <div className="flex justify-evenly">
+                <button
+                    className="bg-cyan-900 hover:bg-cyan-700 text-cyan-100 px-4 py-2 rounded-lg transition duration-200"
+                    onClick={() => setIsInventoryTab(true)}
+                >
+                    Inventory
+                </button>
+                <button
+                    className="bg-cyan-900 hover:bg-cyan-700 text-cyan-100 px-4 py-2 rounded-lg transition duration-200"
+                    onClick={() => setIsInventoryTab(false)}
+                >
+                    Add Gear
+                </button>
             </div>
+            {isInventoryTab ? <Inventory /> : <AddGear />}
         </div>
     );
 };

bebb6dff78d4cb84a96df0a1c2f1062334899d17
Eric Warren <eswarren18@gmail.com>
2025-03-27 12:58:11 -0600

    chore: delete unused files after merging in main

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,15 +1,4 @@
-import {
-    highlightCams,
-    observeAdditionalContent,
-} from './highlight-cam-object';
-// import { createSizeComparisonPopup } from './size-comparison-popup';
-
-function injectCSS() {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = chrome.runtime.getURL('styles.css'); // Path to the CSS file in the `dist` directory
-    document.head.appendChild(link);
-}
+import { searchForCams, observeAdditionalContent } from './highlight-cam-object';
 
 export default defineContentScript({
     // Only matches paths where user will discuss climbing gear

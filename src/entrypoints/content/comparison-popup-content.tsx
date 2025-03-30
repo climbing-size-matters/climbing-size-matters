693805bb2fda51bbe91cdf39eb92fdf87cc2d566
Eric Warren <eswarren18@gmail.com>
2025-03-30 17:17:12 -0600

    chore: clean up CSS in comparison popup

---
+++ src/entrypoints/content/comparison-popup-content.tsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from 'react';
+import { cams } from '../../cam-database/cams';
+import { Cam, Database } from '../../cam-database/types';
+
+type ComparisonPopupProps = {
+    id: string;
+};
+
+export default function ComparisonPopup({ id }: ComparisonPopupProps) {
+    const [displayCam, setDisplayCam] = useState<Cam | null>(null);
+    const [camsInRange, setCamsInRange] = useState<Cam[]>([]);
+    const [ownsCam, setOwnsCam] = useState<boolean>(false);
+
+    const fetchData = async (id: string): Promise<void> => {
+        const cam = cams.find((cam) => cam.id === id);
+        if (cam) {
+            setDisplayCam(cam);
+        }
+    };
+
+    const fetchInventory = async () => {
+        setOwnsCam(false);
+
+        chrome.storage.local.get(['inventory'], (result) => {
+            const currentInventory: Database = result.inventory || {};
+
+            if (displayCam) {
+                for (const brand of currentInventory.brands) {
+                    for (const model of brand.models) {
+                        for (const cam of model.cams) {
+                            if (
+                                cam.model !== displayCam.model &&
+                                Math.abs(
+                                    cam.size.inches[0] -
+                                        displayCam.size.inches[0]
+                                ) <=
+                                    displayCam.size.inches[0] * 0.25 &&
+                                Math.abs(
+                                    cam.size.inches[1] -
+                                        displayCam.size.inches[1]
+                                ) <=
+                                    displayCam.size.inches[1] * 0.25 &&
+                                Math.abs(
+                                    cam.size.inches[0] -
+                                        displayCam.size.inches[0]
+                                ) <=
+                                    cam.size.inches[0] * 0.25 &&
+                                Math.abs(
+                                    cam.size.inches[1] -
+                                        displayCam.size.inches[1]
+                                ) <=
+                                    cam.size.inches[1] * 0.25
+                            ) {
+                                setCamsInRange((prev) => [...prev, cam]);
+                            }
+                            if (cam.id === displayCam.id) {
+                                setOwnsCam(true);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    };
+
+    useEffect(() => {
+        fetchData(id); // Fetch the cam data when `id` changes
+    }, [id]);
+
+    useEffect(() => {
+        if (displayCam) {
+            fetchInventory(); // Fetch inventory only when `displayCam` is updated
+        }
+    }, [displayCam]);
+
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                backgroundColor: 'white',
+                padding: '1rem',
+                borderRadius: '0.5rem',
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                zIndex: 50,
+                border: '1px solid #d1d5db',
+                height: '100%',
+                width: '100%',
+            }}
+        >
+            {displayCam && (
+                <div>
+                    {/* Displayed Cam Info */}
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            flexWrap: 'wrap',
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: '4px',
+                            }}
+                        >
+                            <div style={{ fontWeight: 'bold' }}>Displayed:</div>
+                            {ownsCam && '(Got it!)'}
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                alignItems: 'center',
+                                gap: '4px',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    height: '12px',
+                                    width: '12px',
+                                    borderRadius: '2px',
+                                    border: '1px solid black',
+                                    backgroundColor: displayCam.color,
+                                }}
+                            ></div>
+                            <div>{displayCam.name}</div>
+                            <div>{displayCam.brand}</div>
+                            <div>{displayCam.model}</div>
+                        </div>
+
+                        {/* User Cam Info */}
+                        <div style={{ fontWeight: 'bold' }}>
+                            My comparable gear:
+                        </div>
+                        {camsInRange.map((cam) => (
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexWrap: 'wrap',
+                                    alignItems: 'center',
+                                    gap: '4px',
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        height: '12px',
+                                        width: '12px',
+                                        borderRadius: '2px',
+                                        border: '1px solid black',
+                                        backgroundColor: cam.color,
+                                    }}
+                                ></div>
+                                <div>{cam.name}</div>
+                                <div>{cam.brand}</div>
+                                <div>{cam.model}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}

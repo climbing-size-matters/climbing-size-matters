0620b2bea641ebb76e90eb5f93c41ca466c2f9b0
Eric Warren <eswarren18@gmail.com>
2025-04-03 12:58:35 -0600

    chore: finalized splitting branch for model-data (#19)

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,4 +1,7 @@
-import { searchForCams, observeAdditionalContent } from './highlight-cam-objects';
+import {
+    searchForCams,
+    observeAdditionalContent,
+} from './highlight-cam-objects';
 
 export default defineContentScript({
     // Only matches paths where user will discuss climbing gear

bc9141811f8c7be04adee0c67c9b95d4cfc5c437
Eric Warren <eswarren18@gmail.com>
2025-04-03 12:43:11 -0600

    splitting branches into separate PRs

---
+++ src/entrypoints/content/highlight-cam-objects.ts
@@ -0,0 +1,51 @@
+import { database } from '../../cam-database/database';
+
+// Returns a string of text with HTML color spans around highlighted words
+function highlightCams(text: string): string {
+    for (const cam of database.cams) {
+        const { regex, color } = cam;
+        text = text.replace(
+            regex,
+            `<span data-cam='highlight' style='background-color: ${color} padding: 1px 2px;'>$&</span>`
+        );
+    }
+    return text;
+}
+
+// Function to recursively search and highlight the cam instances
+function searchForCams(element: Node): void {
+    if (element.hasChildNodes()) {
+        if ((element as HTMLElement).dataset.cam === 'highlight') return;
+        element.childNodes.forEach(searchForCams);
+    } else if (element.nodeType === Node.TEXT_NODE) {
+        const highlightedHTML = highlightCams(element.textContent ?? '');
+
+        const highlightedNode = document.createElement('span');
+        highlightedNode.innerHTML = highlightedHTML;
+
+        (element as ChildNode).replaceWith(highlightedNode);
+    }
+}
+
+// Recursively search and highlight cam instances once comments load on a page
+function observeAdditionalContent(): void {
+    const commentList = document.querySelector('.comment-list');
+    if (commentList) {
+        const config = { childList: true };
+
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach((mutation) => {
+                if (
+                    mutation.type === 'childList' &&
+                    mutation.addedNodes.length > 0
+                ) {
+                    searchForCams(commentList);
+                }
+            });
+        });
+
+        observer.observe(commentList, config);
+    }
+}
+
+export { highlightCams, searchForCams, observeAdditionalContent };
--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,4 +1,4 @@
-import { searchForCams, observeAdditionalContent } from './highlight-gear';
+import { searchForCams, observeAdditionalContent } from './highlight-cam-objects';
 
 export default defineContentScript({
     // Only matches paths where user will discuss climbing gear
